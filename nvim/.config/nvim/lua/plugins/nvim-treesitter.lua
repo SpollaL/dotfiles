1ef7cda5e418795bd3516aa282efafe3864b1173
luca spolladore <luca.spolladore@outlook.it>
2025-12-23 18:38:57 +0100

    feat: add markdown viewer and add postgres to tree-sitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,7 @@ return {
     config = function() 
       local config = require('nvim-treesitter.configs')
       config.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "go" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "go", "sql" },
         highlight = {enable = true},
         indent = {enable = true},
       })
